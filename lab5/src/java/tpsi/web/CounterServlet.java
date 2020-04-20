8c9e49f8c21890c1ffca7128bf3b7f23fceccb29
ThomsonZS <33499972+ThomsonZS@users.noreply.github.com>
2020-04-20 11:40:38 +0200

    Add files via upload

--- lab5/src/java/tpsi/web/CounterServlet.java
+++ lab5/src/java/tpsi/web/CounterServlet.java
@@ -9,6 +9,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import lab4.Person;
 
 @WebServlet(name = "HelloServlet", urlPatterns = {"/licznik"})
     public class CounterServlet extends HttpServlet {
@@ -20,12 +21,15 @@ import javax.servlet.http.HttpSession;
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
         HttpSession session = request.getSession(); 
-        int count = 1;     
+        int count = 1;    
         
-        if (!session.isNew()) { 
-            count = (int) session.getAttribute("count") +1;
+        try {
+            if (session.getAttribute("count") != null)
+                count = (int) session.getAttribute("count") +1;
+        } catch (Exception ex) {
             
-        }
+        } 
+
         session.setAttribute("count", count); 
         
         request.getRequestDispatcher("licznik.jsp").forward(request, response);
